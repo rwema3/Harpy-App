87681688404b5c201a34dc85521c79e72779ef50
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-08 08:18:59 +0200

    latitude= null

--- lib/components/common/find_trends_locations/widgets/find_location_dialog.dart
+++ lib/components/common/find_trends_locations/widgets/find_location_dialog.dart
@@ -27,12 +27,7 @@ class _FindLocationDialogState extends State<FindLocationDialog> {
 
           if (tabController.index == 0) {
             Navigator.of(context).pop();
-          } else {
-            setState(() {
-              _validForm = false;
-              _latitude = null;
-              _longitude = null;
-            });
+      
 
             // wait for the view to rebuild before navigating away
             // this ensures that the confirm button gets disabled
